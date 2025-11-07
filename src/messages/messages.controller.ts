115eca5e5c24ccc731deed0b969a902cd5864667
Edet Asuquo <eddy1759@gmail.com>
2025-11-07 15:36:25 +0100

    feat: added message endpoint

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,16 @@
+import { GetUser } from './get-user.decorator';
+
+@Controller('messages')
+export class MessagesController {
+  @Get(':id')
+  getMessageThread(@Param('id') messageId: string) {
+    return this.prisma.message.findUnique({
+      where: { id: messageId },
+    });
+  }
+
+  @Post()
+  sendMessage(@GetUser() user: User) {
+    // ...
+  }
+}
